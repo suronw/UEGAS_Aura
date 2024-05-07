55058ab31d01fec33c7022a61b2870c89455605e
MightySuron <suronw@yahoo.com>
2024-05-07 16:10:39 -0700

    The Base Character Class redo

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -1,4 +1,4 @@
-// Copyright MightySuron
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,22 @@
+// Copyright MightySuron
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
